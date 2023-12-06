0d70eb2ff82026cca30587e4644ff253a79d8b06
AnnamPrasuna <AnnamPrasuna@DESKTOP-TUNPDV2>
2023-12-06 10:54:51 +0530

    Add project files.

---
+++ WebApplication5/WebForm1.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Id&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Name&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Gender&nbsp;
+            <asp:RadioButton ID="RadioButton1" runat="server" GroupName="g" OnCheckedCanged="RadioButton1_CheckedChanged" Text="Male" />
+&nbsp;&nbsp;&nbsp;
+            <asp:RadioButton ID="RadioButton2" runat="server" GroupName="g" Text="Female" />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Hobbies&nbsp;&nbsp;
+            <asp:CheckBox ID="CheckBox1" runat="server" OnCheckedChanged="CheckBox1_CheckedChanged" Text="Carroms" />
+&nbsp;&nbsp;&nbsp;
+            <asp:CheckBox ID="CheckBox2" runat="server" Text="Sleeping" OnCheckedChanged="CheckBox2_CheckedChanged" />
+&nbsp;&nbsp;
+            <asp:CheckBox ID="CheckBox3" runat="server" Text="Eating" OnCheckedChanged="CheckBox3_CheckedChanged" />
+&nbsp;&nbsp;&nbsp;
+            <asp:CheckBox ID="CheckBox4" runat="server" Text="Reading" OnCheckedChanged="CheckBox4_CheckedChanged" />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; State&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownList1" runat="server" Height="60px" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" Width="265px">
+
+            </asp:DropDownList>
+            <br />
+&nbsp;
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="submit" />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication5/WebForm1.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string state = " ";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Add items to the DropDownList
+                DropDownList1.Items.Add(new ListItem("--Select--"));
+                DropDownList1.Items.Add(new ListItem("Andhrapradesh"));
+                DropDownList1.Items.Add(new ListItem("Telangana"));
+                DropDownList1.Items.Add(new ListItem("Kerala"));
+
+                if (DropDownList1.SelectedIndex <= 3)
+                {
+                    state += DropDownList1.Items;
+                }
+
+
+                // Add more items as needed
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(TextBox1.Text);
+            string name = TextBox2.Text;
+            string gender = " ";
+           
+            if (RadioButton1.Checked == true)
+            {
+                gender="male";
+            }
+            if(RadioButton2.Checked==true)
+            {
+                gender="female";
+            }
+
+           
+            //inserting a record using connection oriented architecture
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dotnet"].ToString());
+            con.Open();
+            string q = "insert into emp values('"+id+"','"+name+"','"+gender+ "','"+hobbies+"','"+state+"')";
+            SqlCommand cmd=new SqlCommand(q,con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+        string hobbies = " ";
+
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            string hobby1 = " ";
+           // string hobbies = " ";
+            if (CheckBox1.Checked == true)
+            {
+                hobby1 = "Carroms";
+                hobbies = hobby1 + " ";
+            }
+
+        }
+
+        protected void CheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            string hobby2 = " ";
+            if (CheckBox2.Checked == true)
+            {
+                hobby2 = "Sleeping";
+                hobbies += hobby2 + " ";
+            }
+
+        }
+
+        protected void CheckBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            string hobby3 = " ";
+            if (CheckBox3.Checked == true)
+            {
+                hobby3 = "Eating";
+                hobbies += hobby3 + " ";
+            }
+        }
+
+        protected void CheckBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            string hobby4 = " ";
+            if (CheckBox4.Checked == true)
+            {
+                hobby4 = "Reading";
+                hobbies += hobby4 + " ";
+            }
+
+        }
+        
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            state = DropDownList1.SelectedItem.Text;
+        }
+    }
+}
